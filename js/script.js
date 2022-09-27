fe661435659621b1e8d683e387d705362719e8eb
Braxton28 <braxtontejkl7@gmail.com>
2022-09-27 12:07:39 -0400

    modified JS File

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let str = `Supercalifragilisticexpialidocious`;
+console.log(str.length);
+//Expected output: 34
+
+let a = 7;
+console.log(a);
+
+const bigInt = BigInt (Number.MAX_SAFE_INTEGER);
+console.log(bigInt);
