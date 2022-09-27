dab5d612ddffa2ae41eca1567034b87b8f973bd9
Braxton28 <braxtontejkl7@gmail.com>
2022-09-27 18:56:52 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-let str = `Supercalifragilisticexpialidocious`;
-console.log(str.length);
-//Expected output: 34
-
-let a = 7;
-console.log(a);
-
-const bigint = 12345678901896532147759012345678901234567890n;
-console.log(bigint);
+let text = "Supercalifragilisticexpialidocious";
+console.log(`length of the text:`, text.length);
+let myInt = 7;
+let myBigInt = 12345678901896532147759012345678901234567890n;
+console.log(`my favorite number:`, myInt);
+console.log(BigInt);
+console.log(typeof myInt);
+console.log(typeof myBigInt);
